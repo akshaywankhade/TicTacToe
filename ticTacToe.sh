705f186a983e1682fd6e567b611e0103afa12e4d
Akshay Wankhade <akshuwankhade12@gmail.com>
2020-04-12 17:13:05 +0530

    ADDED: Showing the board to the player

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -39,7 +39,14 @@ function toss(){
 		player=$CPU
 	fi
 }
-
+#FUNCTION TO PRINT BOARD
+function print(){
+	echo "r\c 0 1 2"
+	echo "0   ${board[0]} ${board[1]} ${board[2]}"
+	echo "1   ${board[3]} ${board[4]} ${board[5]}"
+	echo "2   ${board[6]} ${board[7]} ${board[8]}"
+}
 displayBoard
 assign
 toss
+print
