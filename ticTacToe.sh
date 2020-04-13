8350c2dbbce3a61fa2fbad6a13f30a134c3d4f84
Akshay Wankhade <akshuwankhade12@gmail.com>
2020-04-13 09:32:50 +0530

    ADDED: Checking Opponents winning move and Block

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -2,6 +2,11 @@
 
 echo "..........WELCOME TO TIC-TAC-TOE.................."
 
+#VARIABLES
+winMove="."
+blockMove="."
+
+#FUNCTION TO RESET THE BOAED
 declare -a board
 board=( 1 2 3 4 5 6 7 8 9 )
 
@@ -59,11 +64,22 @@ function playerPlay(){
 		echo "You can't place There."
 	fi
 }
-
+#FOR CPU(COMPUTER)
 function cpuPlay(){
-	x=$(( RANDOM%3 ))
-	y=$(( RANDOM%3 ))
-	local index=$(( $x*3+$y ))
+	local index=0
+	checkWinMove
+	checkBlockMove
+	if [ $winMove != "." ]
+	then
+		index=$winMove
+	elif [ $blockMove != "." ]
+	then
+		index=$blockMove
+	else
+		x=$(( RANDOM%3 ))
+		y=$(( RANDOM%3 ))
+		local index=$(( $x*3+$y ))
+	fi
 	if [ ${board[$index]} == "." ]
 	then
 		board[$index]=$CPU
@@ -72,7 +88,7 @@ function cpuPlay(){
 	fi
 }
 
-#FUNCTION TO CHECK WIN
+#FUNCTSSSION TO CHECK WIN
 function check(){
 	if [ ${board[$1]} != "." ] && [ ${board[$1]} == ${board[$2]} ] && [ ${board[$2]} == ${board[$3]} ]
 	then
@@ -119,35 +135,76 @@ function checkWinMove(){
 	checkMove 0 4 8
 	checkMove 2 4 6
 }
+#FUNCTION TO CHECK Opponent WIN MOVE AND BLOCK IT
+function checkOpMove(){
+	if [ ${board[$1]} == "." ] && [ ${board[$2]} == $PLAYER ] && [ ${board[$3]} == $PLAYER ] ||
+		[ ${board[$2]} == "." ] && [ ${board[$1]} == $PLAYER ] && [ ${board[$3]} == $PLAYER ] ||
+		[ ${board[$3]} == "." ] && [ ${board[$2]} == $PLAYER ] && [ ${board[$1]} == $PLAYER ]
+	then
+		if [ ${board[$1]} == "." ]
+		then
+			blockMove=$1
+		elif [ ${board[$2]} == "." ]
+		then
+			blockMove=$2
+		elif [ ${board[$3]} == "." ]
+		then
+			blockMove=$3
+		fi
+	fi
+}
+
+function checkBlockMove(){
+	checkOpMove 0 1 2
+	checkOpMove 3 4 5
+	checkOpMove 6 7 8
+	checkOpMove 0 3 6
+	checkOpMove 1 4 7
+	checkOpMove 2 5 8
+	checkOpMove 0 4 8
+	checkOpMove 2 4 6
+}
+
+
 #PLAYING GAME
 reset
 assign
 toss
-if [ $player -eq $PLAYER ]
-then
-	playerPlay
-elif [ $player -eq $CPU ]
-then
-	cpuPlay
-fi
 print
-gameCheck
-playerPlay
-cpuPlay
-
-#CHECKING WIN OR TIE OR TURN PLAYER
-if [ $gameStatus != 1 ]
-then
-	echo "Game Over"
-	echo "$player Win"
-elif [ $gameStatus -eq 1 ] && [ $turnCounter -eq 9 ]
-then
-	echo "Its A Tie"
-else
+while [ true ]
+do
+	(( turnCounter++ ))
 	if [ $player -eq $PLAYER ]
 	then
-		player=$CPU
-	else
-		player=$PLAYER
+		playerPlay
+	elif [ $player -eq $CPU ]
+	then
+		cpuPlay
 	fi
-fi
+
+	gameCheck
+	playerPlay
+	cpuPlay
+
+	#CHECKING WIN OR TIE OR TURN PLAYER
+	if [ $gameStatus != 1 ]
+	then
+		echo "Game Over"
+		echo "$player Win"
+	elif [ $gameStatus -eq 1 ] && [ $turnCounter -eq 9 ]
+	then
+		echo "Its A Tie"
+	else
+		if [ $player -eq $PLAYER ]
+ 		then
+			player=$CPU
+		else
+			player=$PLAYER
+		fi
+	fi
+	
+	if [ $turnCounter -eq 9 ]
+	then
+		break
+	fi
+done
